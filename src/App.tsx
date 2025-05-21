0ca626c31ac4975d57adce760da92a99a31a93b9
최혜림 <101788177+hyerim03@users.noreply.github.com>
2025-05-21 10:19:10 +0900

    fix : 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Route, Routes } from "react-router-dom";
-import "./App.css";
 import LoginContainer from "./pages/login/LoginContainer";
 import RegisterContainer from "./pages/register/RegisterContainer";
 import AllMonitoringContainer from "./pages/all- monitoring/AllMonitoringContainer";
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
-import "./App.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
