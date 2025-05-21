d1d81ee4186c617b6f5e32a6572714ec901b7342
최혜림 <101788177+hyerim03@users.noreply.github.com>
2025-05-21 17:31:52 +0900

    fix: 오류 해결 시도

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,5 @@
-module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-  ],
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
