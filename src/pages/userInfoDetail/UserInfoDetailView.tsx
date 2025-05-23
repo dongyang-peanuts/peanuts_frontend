d1f1d217f8ae08c89c15a4a7b7a6b8351d64d806
최혜림 <101788177+hyerim03@users.noreply.github.com>
2025-05-23 17:22:42 +0900

    feat : 사용자 정보 상세 수정중

--- src/pages/userInfoDetail/UserInfoDetailView.tsx
+++ src/pages/userInfoDetail/UserInfoDetailView.tsx
@@ -5,7 +5,7 @@ const UserInfoDetailView = () => {
         <div>전체 사용자 정보 조회</div>
         <div>
           <div>보호자 정보</div>
-          <div></div>
+          <hr />
           <div>
             <div>보호자 이메일</div>
             <div>chhari0708@naver.com</div>
@@ -13,6 +13,58 @@ const UserInfoDetailView = () => {
           <div>
             <div>보호자 전화번호</div>
             <div>010-7132-8582</div>
+          </div>
+          <div>
+            <div>주소</div>
+            <div>chhari0708@naver.com</div>
+          </div>
+          <div>
+            <div>보호자 이메일</div>
+            <div>chhari0708@naver.com</div>
+          </div>
+        </div>
+
+        <div>
+          <div>환자 정보</div>
+          <hr />
+          <div>
+            <div>환자 이름</div>
+            <div>최혜림</div>
+          </div>
+          <div>
+            <div>환자 나이</div>
+            <div>81세</div>
+          </div>
+          <div>
+            <div>환자 키</div>
+            <div>165cm</div>
+          </div>
+          <div>
+            <div>환자 체중</div>
+            <div>55kg</div>
+          </div>
+        </div>
+
+        <div>
+          <div>환자 활동 정보</div>
+          <hr />
+          <div>
+            <div>예상질병</div>
+            <div>
+              <div>알츠하이머</div>
+              <div>치매</div>
+              <div>우울증</div>
+              <div>신경계 질환</div>
+            </div>
+          </div>
+          <div>
+            <div>복용중인 약</div>
+            <div>
+              <div>이부프로펜(Advil) </div>
+              <div>나프록센(Aleve)</div>
+              <div>멜록시캄(Mobic)</div>
+              <div>셀레콕시브(Celebrex)</div>
+            </div>
           </div>
           <div>
             <div>주소</div>
